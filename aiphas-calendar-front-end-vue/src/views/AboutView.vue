fa61263057fad546d723094573bfe625a86c5031
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-08-12 16:54:27 +0800

    test bootstrap

---
+++ aiphas-calendar-front-end-vue/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+     <!--tes bootstrap button -->
+    <button type="button" class="btn btn-info">Info</button>
+  </div>
+</template>
