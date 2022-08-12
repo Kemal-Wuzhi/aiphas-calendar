075358d8858b2ca3282548ca3dccc45cd5004512
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-08-12 18:12:29 +0800

    feat:add signin and signup page

--- aiphas-calendar-front-end-vue/src/router/index.js
+++ aiphas-calendar-front-end-vue/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
+import SignUp from '../views/SignUp.vue'
 
 Vue.use(VueRouter)
 
@@ -9,7 +11,18 @@ const routes = [
     path:'*',
     name:'not-found',
     component:NotFound,
-  }
+  },
+  {
+    path:'/signin',
+    name:'sign-in',
+    component:SignIn
+  },
+  {
+    path:'/signup',
+    name:'sign-up',
+    component:SignUp
+  },
+  
 ]
 
 const router = new VueRouter({
---
+++ aiphas-calendar-front-end-vue/src/views/SignUp.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container py-5">
+    <form class="w-100" @submit.prevent.stop="handleSubmit">
+      <div class="text-center mb-4">
+        <h1 class="h3 mb-3 font-weight-normal">
+          Sign Up
+        </h1>
+      </div>
+
+      <div class="form-label-group mb-2">
+        <label for="email">Email</label>
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="Email"
+          required
+          autofocus
+        >
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="name">Name</label>
+        <input
+          id="name"
+          v-model="name"
+          name="name"
+          type="name"
+          class="form-control"
+          placeholder="Name"
+          required
+        >
+      </div>
+
+      <div class="form-label-group mb-3">
+        <label for="password">Password</label>
+        <input
+          id="password"
+          v-model="password"
+          name="password"
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          required
+        >
+      </div>
+
+      <button
+        class="btn btn-lg btn-primary btn-block mb-3"
+        type="submit"
+      >
+        Submit
+      </button>
+
+      <p class="mt-5 mb-3 text-muted text-center">
+ 
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      name: '',
+      password: ''
+      
+    }
+  },
+  methods:{
+    handleSubmit(){
+        const data = JSON.stringify({
+            email:this.email,
+            name:this.name,
+            password:this.password
+        })
+    console.log(data)
+    }
+  }
+}
+</script>
