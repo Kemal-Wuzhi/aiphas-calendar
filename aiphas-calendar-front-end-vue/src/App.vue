627f646ccdcc3302b95583f2dff94816dae45b1d
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-08-12 17:16:11 +0800

    refactor: delete Home and About commponents

--- aiphas-calendar-front-end-vue/src/App.vue
+++ aiphas-calendar-front-end-vue/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
+
+<!-- import bootstrap -->
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default {
+  name: 'App'
+}
+</script>
 
 <style>
 #app {
--- aiphas-calendar-front-end-vue/src/views/AboutView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-
-     <!--tes bootstrap button -->
-    <button type="button" class="btn btn-info">Info</button>
-  </div>
-</template>
